90f44522122c28d6f30aa7b2633b5b4f1f670a78
kazurayam <kazuaki.urayama@gmail.com>
2025-08-24 21:11:57 +0900

    time.html now displays the counter 1,3,Fizz,4,Buzz,...

--- katalon-studio-project/KS_SmartWait_runs_slow_in_v10.3.1/Scripts/TC1/Script1755999231913.groovy
+++ katalon-studio-project/KS_SmartWait_runs_slow_in_v10.3.1/Scripts/TC1/Script1755999231913.groovy
@@ -1,22 +1,6 @@
-import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
-import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
-import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
-import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
-import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
-import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
-import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
-import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
-import com.kms.katalon.core.model.FailureHandling as FailureHandling
-import com.kms.katalon.core.testcase.TestCase as TestCase
-import com.kms.katalon.core.testdata.TestData as TestData
-import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
-import com.kms.katalon.core.testobject.TestObject as TestObject
-import com.kms.katalon.core.testobject.ConditionType 
-import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.testobject.ConditionType
+import com.kms.katalon.core.testobject.TestObject
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
-import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
-import internal.GlobalVariable as GlobalVariable
-import org.openqa.selenium.Keys as Keys
 
 TestObject makeTestObject(String id, String xpath) {
 	TestObject tObj = new TestObject(id)
@@ -30,6 +14,9 @@ WebUI.openBrowser("")
 WebUI.setViewPortSize(800, 600)
 WebUI.navigateToUrl(url)
 
-WebUI.waitForElementPresent(makeTestObject("FizzBuzz", "//*/div[contains(.,'FizzBuzz')]"), 100)
+WebUI.waitForElementPresent(makeTestObject("fizzbuzz", "//*/div[contains(.,'FizzBuzz')]"), 20)
 
+WebUI.click(makeTestObject("STOP BUTTON", "#btn"))
+
+WebUI.delay(1)
 WebUI.closeBrowser()
